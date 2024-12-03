d1aad4e661ba46ce89f7e61f50b7c079861ee41b
Stefan <qsxvert@gmail.com>
2024-12-03 22:44:17 +0100

    1

---
+++ org/org/mnapp/Bady.java
@@ -0,0 +1,164 @@
+package org.mnapp.app;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.security.KeyStore;
+import java.time.ZonedDateTime;
+
+import javax.net.ssl.SSLContext;
+import com.test.edeklaracje.GateService;
+import com.test.edeklaracje.GateServicePortType;
+import com.test.edeklaracje.RequestUPO;
+import jakarta.xml.ws.Holder; 
+
+public class Bady {
+	
+	Holder<String> refId;
+	Holder<Integer> status;
+	Holder<String> statusOpis;
+	Holder<String> upo;
+//	byte[] document;
+	String id;
+	
+	public Bady(byte[] document, Holder<String> refId, Holder<Integer> status, Holder<String> statusOpis) {
+//		this.document = document;
+		this.refId = refId;
+		this.status = status;
+		this.statusOpis = statusOpis;
+	}
+	
+	public Bady(Holder<String> refId, Holder<Integer> status, Holder<String> statusOpis) {
+		this.refId = refId;
+		this.status = status;
+		this.statusOpis = statusOpis;
+	}
+	
+	public Bady() {
+	}
+	
+		public void sendDoc(byte[] document) {
+			try { 
+				// Załaduj klucz prywatny z keystore 
+				 KeyStore keyStore = KeyStore.getInstance("JKS"); 
+				 keyStore.load(new FileInputStream("/home/tee/eclipse-workspace/keystore.jks"), "qqq111".toCharArray()); 
+				 // Załaduj zaufane certyfikaty z truststore 
+				 KeyStore trustStore = KeyStore.getInstance("JKS"); 
+				 trustStore.load(new FileInputStream("/home/tee/eclipse-workspace/keystore.jks"), "qqq111".toCharArray()); 
+				 // Skonfiguruj SSLContext 
+				 SSLContext sslContext = SSLContext.getInstance("TLS"); 
+				 // Inicjalizuj SSLContext z trustManager i keyManager 
+				
+					GateService service = new GateService(); 
+					GateServicePortType port = service.getGateServiceSOAP11Port(); // Przykładowe wywołanie operacji 
+					
+					//document = new Byte{0,1,0,1};
+					
+					refId = new Holder();
+					status = new Holder();
+					statusOpis = new Holder();
+					
+					port.sendDocument(document, refId, status, statusOpis); 
+					System.out.println("Response: " + refId.value );
+					System.out.println("Response: " + status.value );
+					System.out.println("Response: " + statusOpis.value );
+					
+					BufferedWriter bfw = new BufferedWriter(new FileWriter("/home/tee/test.list", true));
+					bfw.newLine();
+					bfw.write(ZonedDateTime.now().toString() + " " + refId.value + " " + status.value + " " + statusOpis.value);
+					bfw.flush();
+					bfw.close();
+					
+				} catch (Exception e) { 
+						e.printStackTrace();
+				} 
+
+		}
+		
+		
+		public void getUPO() {
+			try { 
+//				// Załaduj klucz prywatny z keystore 
+				 KeyStore keyStore = KeyStore.getInstance("JKS"); 
+				 keyStore.load(new FileInputStream("/home/tee/eclipse-workspace/keystore.jks"), "qqq111".toCharArray()); 
+				 // Załaduj zaufane certyfikaty z truststore 
+				 KeyStore trustStore = KeyStore.getInstance("JKS"); 
+				 trustStore.load(new FileInputStream("/home/tee/eclipse-workspace/keystore.jks"), "qqq111".toCharArray()); 
+				 // Skonfiguruj SSLContext 
+				 SSLContext sslContext = SSLContext.getInstance("TLS"); 
+				 // Inicjalizuj SSLContext z trustManager i keyManager 
+				 
+				
+//				// Utwórz instancję usługi 
+				GateService service = new GateService(); 
+				GateServicePortType port = service.getGateServiceSOAP11Port(); 
+				// Utwórz żądanie UPO 
+				RequestUPO requestUPO = new RequestUPO(); 
+				
+				id = new String("88a60f9d03045f163e10790ace36ebc3");
+				requestUPO.setRefId(id); // Ustaw refID dokumentu 
+				
+				upo = new Holder();
+				status = new Holder();
+				statusOpis = new Holder();
+				
+				// Wywołaj metodę requestUPO 
+				port.requestUPO(id, "pl", upo, status, statusOpis);
+				
+				System.out.println("UPO status: " + upo.value + " " + status.value + " " + statusOpis.value);
+			} catch (Exception e) { 
+				e.printStackTrace(); 
+//				System.out.println("nydyrydy!!!");
+				}
+			System.out.println("end");
+			}
+
+		public Holder<String> getRefId() {
+			return refId;
+		}
+
+		public void setRefId(Holder<String> refId) {
+			this.refId = refId;
+		}
+
+		public Holder<Integer> getStatus() {
+			return status;
+		}
+
+		public void setStatus(Holder<Integer> status) {
+			this.status = status;
+		}
+
+		public Holder<String> getStatusOpis() {
+			return statusOpis;
+		}
+
+		public void setStatusOpis(Holder<String> statusOpis) {
+			this.statusOpis = statusOpis;
+		}
+
+		public Holder<String> getUpo() {
+			return upo;
+		}
+
+		public void setUpo(Holder<String> upo) {
+			this.upo = upo;
+		}
+
+//		public byte[] getDocument() {
+//			return document;
+//		}
+//
+//		public void setDocument(byte[] document) {
+//			this.document = document;
+//		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+	}
---
+++ org/org/mnapp/Run.java
@@ -0,0 +1,73 @@
+package org.mnapp.bin;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Scanner;
+
+import org.mnapp.app.Bady;
+import org.mnapp.app.WsdlToJavaGenerator;
+
+import jakarta.xml.ws.Holder;
+
+
+public class Run {
+
+	public static void main(String[] args) throws IOException {
+		
+		Bady bady = new Bady();
+		
+		System.out.println("1- wysyłka");
+		System.out.println("2- UPO");
+		System.out.println("3- generuj źródła test");
+		Scanner scan = new Scanner(System.in);
+//		System.out.println(scan.next());
+		
+		File file = new File("/home/tee/test1");
+		byte[] docInByteArr = file.toString().getBytes();
+		
+		switch (scan.next().toString()) {
+		case "1" :
+				bady.sendDoc(docInByteArr);
+			break;
+		case "2":
+				bady.getUPO();
+			break;
+		case "3":
+				String wsdl = "https://test-bramka.edeklaracje.gov.pl/uslugi/dokumenty?wsdl";
+				WsdlToJavaGenerator ws2j = new WsdlToJavaGenerator();
+				ws2j.generateTestWsdlSource("test", wsdl);
+			break;
+		case "4":
+
+				BufferedWriter bfw = new BufferedWriter(new FileWriter("/home/tee/test.list", true));
+				bfw.newLine();
+				bfw.write(ZonedDateTime.now().toString());
+				bfw.flush();
+				bfw.close();
+			break;
+		default:
+			break;
+		}
+		
+
+//		for (int i = 0; i < docInByteArr.length; i++) {
+//			System.out.println(docInByteArr[i]);
+//		}
+		
+//		BufferedWriter ot = new BufferedWriter(new FileWriter(file));
+//		byte[] ex = {0,1,0,1};
+//		char[] st = ex.toString().toCharArray();
+//		String a = st.toString();
+//		System.out.println(a);
+//		ot.write(ex.toString().toCharArray());
+//		ot.flush();
+//		ot.close();
+//		file.createNewFile();
+//		file.delete();
+	}
+}
